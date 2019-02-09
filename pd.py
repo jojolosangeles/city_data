32281ac0fbfb73bd1aad747465ac0a2573f1dfac
Joh Johannsen <jojolosangeles2017@gmail.com>
2019-02-08 19:49:56 -0800

    big picture sort of works

--- pd.py
+++ pd.py
@@ -84,9 +84,13 @@ class BarGraphCommand:
         dataFrameName = parameters[0]
         xConfiguration = parameters[1].replace('+', ' ').split('|')
         yConfiguration = parameters[2].replace('+', ' ').split('|')
+        dataFrame = context.get_data_frame(dataFrameName)
+        self.draw_bar_graph(dataFrameName, dataFrame, xConfiguration, yConfiguration)
+
+    def draw_bar_graph(self, dataFrameName, dataFrame, xConfiguration, yConfiguration):
         print("BarGraphCommand({dataFrameName}) {xConfiguration}, {yConfiguration}"
             .format(dataFrameName=dataFrameName,xConfiguration=xConfiguration,yConfiguration=yConfiguration))
-        dataFrame = context.get_data_frame(dataFrameName)
+
         chart = alt.Chart(dataFrame).mark_bar().encode(
             alt.X(xConfiguration[1], title=xConfiguration[2]),
             alt.Y(yConfiguration[1], title=yConfiguration[2])
@@ -94,6 +98,41 @@ class BarGraphCommand:
         imageFileName = "{dataFrameName}.png".format(dataFrameName=dataFrameName)
         chart.save(imageFileName, scale_factor=2.0)
         print("Saved {imageFileName}".format(imageFileName=imageFileName))
+
+class MultiBarGraphCommand:
+    def execute(self, context, *args):
+        if len(*args) < 4:
+            for arg in args:
+                print(" {arg}".format(arg=arg))
+            raise ValueError("Expected 4 parameters: <dataframe name> <group by column name> <X column configuration> <Y column configuration>, got {numargs} parameters".format(numargs=len(*args)))
+        parameters = args[0]
+        dataFrameName = parameters[0]
+        lastParameterOffset = len(parameters) - 1
+        columnDescriminator = parameters[1:(lastParameterOffset-1)]
+        columnDescriminatorString = " ".join(columnDescriminator)
+        print("Column Descriminator = '{columnDescriminatorString}'".format(columnDescriminatorString=columnDescriminatorString))
+        xConfiguration = parameters[lastParameterOffset-1].replace('+', ' ').split('|')
+        yConfiguration = parameters[lastParameterOffset].replace('+', ' ').split('|')
+        print("BarGraphCommand({dataFrameName}) {xConfiguration}, {yConfiguration}"
+            .format(dataFrameName=dataFrameName,xConfiguration=xConfiguration,yConfiguration=yConfiguration))
+
+        uniqueValuesForColumn = context.get_unique_values_for_column(columnDescriminatorString)
+        print("Unique Values For Column = {uniqueValuesForColumn}".format(uniqueValuesForColumn=uniqueValuesForColumn))
+        barGraph = BarGraphCommand()
+        for value in uniqueValuesForColumn:
+            normalized_value = name_normalize(value)
+            dataFrame = context.get_data_frame("{dataFrameName}.{normalized_value}".format(dataFrameName=dataFrameName,normalized_value=normalized_value))
+            print("VALUE={value}".format(value=value))
+            #print(dataFrame.head(3))
+            barGraph.draw_bar_graph(normalized_value, dataFrame, xConfiguration, yConfiguration)
+        # dataFrame = context.get_data_frame(dataFrameName)
+        # chart = alt.Chart(dataFrame).mark_bar().encode(
+        #     alt.X(xConfiguration[1], title=xConfiguration[2]),
+        #     alt.Y(yConfiguration[1], title=yConfiguration[2])
+        # )
+        # imageFileName = "{dataFrameName}.png".format(dataFrameName=dataFrameName)
+        # chart.save(imageFileName, scale_factor=2.0)
+        # print("Saved {imageFileName}".format(imageFileName=imageFileName))
 
 class FilterByCommand:
     def execute(self, context, *args):
@@ -109,13 +148,13 @@ class FilterByCommand:
         print("{dataFrameName} => {columnName}".format(dataFrameName=dataFrameName, columnName=columnName))
         dataFrame = context.get_data_frame(dataFrameName)
         uniqueValuesForColumn = dataFrame[columnName].unique()
+        context.register_unique_values_for_column(columnName, uniqueValuesForColumn)
         print(uniqueValuesForColumn)
         for uniqueValue in uniqueValuesForColumn:
             normalized_value = name_normalize(uniqueValue)
             keyName = "{dataFrameName}.{normalized_value}".format(dataFrameName=dataFrameName, normalized_value=normalized_value)
             filtered_data = dataFrame.loc[dataFrame[columnName] == uniqueValue]
             print("FILTERED to {keyName}".format(keyName=keyName))
-            print(filtered_data.head(3))
             context.put_data_frame(keyName, filtered_data)
 
 class DoCommand:
@@ -154,10 +193,12 @@ class Context:
             "drop": DropColumnCommand(),
             "create": CreateColumnCommand(),
             "bar": BarGraphCommand(),
+            "mbar": MultiBarGraphCommand(),
             "filter": FilterByCommand(),
             "do": DoCommand()
         }
         self.dataFrames = {}
+        self.uniqueValuesForColumn = {}
 
     def get_data_frame(self, dataFrameName):
         return self.dataFrames[dataFrameName]
@@ -165,6 +206,14 @@ class Context:
     def put_data_frame(self, dataFrameName, dataFrame):
         print("put_data_frame({dataFrameName})".format(dataFrameName=dataFrameName))
         self.dataFrames[dataFrameName] = dataFrame
+
+    def register_unique_values_for_column(self, columnName, uniqueValuesForColumn):
+        print("Register UNIQUE VALUES FOR {columnName}".format(columnName=columnName))
+        self.uniqueValuesForColumn[columnName] = uniqueValuesForColumn
+
+    def get_unique_values_for_column(self, columnName):
+        print("REMEMBER UNIQUE VALUES FOR {columnName}".format(columnName=columnName))
+        return self.uniqueValuesForColumn[columnName]
 
     def process_line(self, line):
         data = line.split()
